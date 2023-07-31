f54c90384fb0a67f21b7954b4d3209a2f729e15c
rohan-mostofa <rohanmostofa599@gmail.com>
2023-07-31 09:44:49 +0600

    updated

--- front-end/redux/api/apiSlice.ts
+++ front-end/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath : "api",
-    baseQuery : fetchBaseQuery({ baseUrl : "http://localhost:8080/api/v1", credentials : "include" }),
+    baseQuery : fetchBaseQuery({ baseUrl : "https://portfolio-9853.onrender.com/api/v1", credentials : "include" }),
     endpoints : (builder) => ({
 
         register : builder.mutation({
