d4b5ae7c1e882a7ea5c74357af81a6ec83921825
rohan-mostofa <rohanmostofa599@gmail.com>
2023-07-31 09:55:48 +0600

    updated

--- front-end/components/portfolio-section/Portfolio.tsx
+++ front-end/components/portfolio-section/Portfolio.tsx
@@ -30,8 +30,8 @@ const Portfolio = () => {
 
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[1.90rem] mt-[50px]">
 
-                  { data?.portfolios.map((data: object) => (
-                      <div className="portfolioCard relative">
+                  { data?.portfolios.map((data: object, index: number) => (
+                      <div className="portfolioCard relative" key={index}>
                         <span className="px-4 py-1 text-[19px] font-medium uppercase rounded-lg shadow-lg text-white bg-[#28AE60] absolute top-3 right-3 z-50">{data.status}</span>
 
                         <Link target='_blank' href={data.projectUrl}>
